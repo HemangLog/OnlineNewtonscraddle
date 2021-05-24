d7535e870e8db71f52895d0b071c341883847463
HemangLog <73376030+HemangLog@users.noreply.github.com>
2021-05-24 23:54:43 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,3 @@
-//@hemangPresents
-
-//Name of the effort = Online Newton's craddle
-
-//Description
-//It's a small creation for Science thinkers, lovers and enthusiasts, 
-//that gives an opportunity and chance to view and perform the Newton's
-//Craddle at online media.
-
-//Objective
-//To solve the questions and misunderstandings about Newton's craddle
-//through live online motion of it. And, help them to know more about 
-//physics.
-
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
